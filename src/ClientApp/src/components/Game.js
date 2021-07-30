d279bd587bd85cc8185dd5b87cfb509b728352b3
Vinnie <vinnie@pepperpants.nl>
2021-07-30 19:27:24 +0200

    set player

--- src/ClientApp/src/components/Game.js
+++ src/ClientApp/src/components/Game.js
@@ -13,13 +13,17 @@ class Board extends Component {
     super(props);
     this.state = {
       squares:Array(9).fill(null),
+      playerOne:true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.playerOne ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      playerOne: !this.state.playerOne,
+    });
   }
 
   renderSquare(i) {
@@ -31,7 +35,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.playerOne ? 'X' : 'O');
 
     return (
       <div>
