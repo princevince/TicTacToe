98e67286c8f604b83aecf3574fbe5fed4037ea2b
Vinnie <vinnie@pepperpants.nl>
2021-07-30 19:03:18 +0200

    Keep game state in board class

--- src/ClientApp/src/components/Game.js
+++ src/ClientApp/src/components/Game.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares:Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />);
   }
 
   render() {
